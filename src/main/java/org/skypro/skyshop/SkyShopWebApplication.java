7c56871381b76d2179dafe42b9bdf0888dc4bfe5
Konstantin Terskikh <kostus.online@gmail.com>
2024-12-20 12:59:10 +0300

    Initial commit

---
+++ src/main/java/org/skypro/skyshop/SkyShopWebApplication.java
@@ -0,0 +1,16 @@
+// SkyPro
+// Терских Константин, kostus.online.1974@yandex.ru, 2024
+// Домашнее задание по теме "Жизненный цикл компонентов Spring Boot приложения"
+
+package org.skypro.skyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyShopWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyShopWebApplication.class, args);
+	}
+}
